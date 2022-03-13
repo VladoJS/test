086114f24d5faa2c9d823eb8812d874c2c36cb6c
Vlad <example@gmail.com>
2022-03-13 21:11:20 +0300

    worked out event delegation

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 const btns = document.querySelectorAll('button');
+
 btns[0].addEventListener('click', () => {
-    // if (!btns[1].classList.contains('red')) {
-    //     btns[1].classList.add('red');
-    // } else {
-    //     btns[1].classList.remove('red');
-    // }
+//     // if (!btns[1].classList.contains('red')) {
+//     //     btns[1].classList.add('red');
+//     // } else {
+//     //     btns[1].classList.remove('red');
+//     // }
 
     btns[1].classList.toggle('red');
 });
+
+// Делегирование событий:
+
+const wrapper = document.querySelector('.btn-block');
+
+wrapper.addEventListener('click', (event) => {
+    if (event.target && event.target.matches("button.red")) {
+        console.log('HELLO');
+    }
+});
+
+// btns.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         console.log('hello');
+//     });
+// });
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);

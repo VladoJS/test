ec175063013df68851e731cb92cdd607d29a98f3
Vlad <example@gmail.com>
2022-03-13 18:25:34 +0300

    classList

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const btns = document.querySelectorAll('button');
+btns[0].addEventListener('click', () => {
+    // if (!btns[1].classList.contains('red')) {
+    //     btns[1].classList.add('red');
+    // } else {
+    //     btns[1].classList.remove('red');
+    // }
 
-console.log(btns[0].classList.length);
+    btns[1].classList.toggle('red');
+});
